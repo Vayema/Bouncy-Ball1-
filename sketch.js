04997221b30dcc94bc80df257e68514200dcac5f
Vayema <73804087+Vayema@users.noreply.github.com>
2020-11-27 18:43:23 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var ground, box,box2;
+
+function setup(){
+    var canvas = createCanvas(400,400);
+    engine = Engine.create();
+    world = engine.world;
+box=new Box(200,200,50,80);
+box2=new Box(240,300,80,50);
+ground= new Ground(200,390,400,50)   
+}
+
+function draw(){
+    background(0);
+    Engine.update(engine);
+    box.display();
+    box2.display();
+    ground.display();
+   
+}
